ccadc2660aa3dc7f2b01ed23b95da1568235a959
Giorgio-PC <x.file.gt@gmail.com>
2024-01-04 14:00:54 +0100

    fixes routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 export default function App() {
   return (
