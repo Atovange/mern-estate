c1726cea8998986a646b7ecd7cbe93673c6ed7ee
Giorgio-PC <x.file.gt@gmail.com>
2024-01-04 18:16:24 +0100

    complete sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,20 @@
 import bcryptjs from "bcryptjs";
 
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
+import {errorHandler} from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
+    if (
+        !username ||
+        !email ||
+        !password ||
+        email === '' ||
+        password === '' ||
+        username === ''
+      ) {
+        return next(errorHandler(400, 'All fields are required!'));
+    }
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password: hashedPassword});
     try {
