e5696677908ab8c8d7216fb9f1cb8b06d7ffcfdb
Giorgio-PC <x.file.gt@gmail.com>
2024-01-04 17:43:25 +0100

    create error middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,16 +2,14 @@ import bcryptjs from "bcryptjs";
 
 import User from "../models/user.model.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password: hashedPassword});
-    await newUser.save()
-        .then(dbResponse => {
-            res.status(201).json({message: "User created successfully", db: dbResponse});
-        })
-        .catch(error => {
-            res.status(400).json({error: error.message});
-        });
-    
+    try {
+        await newUser.save();
+        res.status(201).json({message: "User created successfully", db: dbResponse});
+    } catch(error) {
+        next(error);
+    }
 }
