8a535cadffc28d4969bf596cc1553f93355a12d1
Giorgio-PC <x.file.gt@gmail.com>
2024-01-14 17:30:03 +0100

    loading message listing update

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -172,13 +172,10 @@ export default function CreateListing() {
 		</div>
 	);
 
-	if (isFetchingListing)
-		return "Loading..."
-
 	return (
 		<main className='p-3 max-w-4xl mx-auto'>
 			<h1 className='text-3xl text-center font-semibold my-7'>
-				{id ? "Update listing" : "Create a Listing"}
+				{isFetchingListing ? "Loading..." : (id ? "Update listing" : "Create a Listing")}
 			</h1>
 			<form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-5'>
 				<div className='flex flex-col gap-4 flex-1'>
