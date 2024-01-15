f03b493450e4ae2fc9cd7765f09cd19e705f36e1
Giorgio-PC <x.file.gt@gmail.com>
2024-01-15 02:18:18 +0100

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-5xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Us</h1>
+      <p className='mb-8 text-slate-700 text-lg'>Welcome to GiorgioEstate, your trusted destination for finding the ideal home that resonates with your lifestyle and aspirations.</p>
+      <h3 className='text-3xl font-bold mb-4 text-slate-800'>Our Mission</h3>
+      <p className='mb-8 text-slate-700 text-lg'>At GiorgioEstate, we are driven by the mission to simplify and enhance the home-search experience. Whether you're looking for a chic urban apartment, a family-friendly suburban house, or an opulent penthouse, we have a diverse range of listings to cater to every taste.</p>
+      <h3 className='text-3xl font-bold mb-4 text-slate-800'>What Sets Us Apart</h3>
+      <p className='mb-8 text-slate-700 text-lg'>
+      <ul className='list-disc'>
+        <li>
+            <strong>Curated Selection:</strong> We believe in quality over quantity. Our listings are handpicked to ensure they meet high standards of design, functionality, and value.
+        </li>
+        <li>
+            <strong>User-Friendly Platform:</strong> Navigating the journey to your dream home should be seamless. Our user-friendly platform is designed to make the search process efficient and enjoyable.
+        </li>
+        <li>
+            <strong>Transparent Transactions:</strong> We prioritize transparency in every transaction. Clear pricing, detailed property information, and honest representation of each listing ensure a trustworthy experience.
+        </li>
+        <li>
+            <strong>Dedicated Support:</strong> Our team is here to assist you at every step. From initial exploration to the final decision, our dedicated support ensures you have the guidance you need.
+        </li>
+      </ul>
+
+      </p>
+    </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {Link} from "react-router-dom";
+import ListingCard from '../components/ListingCard';
 
 export default function Home() {
+  const [offersListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const res1 = await fetch("/api/listings?limit=4&offer=true");
+      const data1 = await res1.json();
+      setOfferListings(data1);
+      const res2 = await fetch("/api/listings?limit=4&sell=true");
+      const data2 = await res2.json();
+      setSaleListings(data2);
+      const res3 = await fetch("/api/listings?limit=4&rent=true");
+      const data3 = await res3.json();
+      setRentListings(data3);
+    }
+    fetchListings();
+  }, []);
+
+  const offersListingsCards = offersListings.map(listing => 
+    <ListingCard key={listing._id} listing={listing}/>
+  );
+  const saleListingsCards = saleListings.map(listing => 
+    <ListingCard key={listing._id} listing={listing}/>
+  );
+  const rentListingsCards = rentListings.map(listing => 
+    <ListingCard key={listing._id} listing={listing}/>
+  );
+
   return (
-    <div>Home</div>
+    <div>
+      <div className='flex flex-col gap-5 p-28 px-5 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+          Discover you next <span className='text-slate-500'>perfect</span><br/>
+          home with ease
+        </h1>
+        <p className='text-gray-500 text-xl sm:text-lg'>
+          Explore a curated collection of premium estates, from luxurious condos to cozy family homes.<br/>
+          Find the perfect blend of comfort and style with our meticulously crafted listings.<br/>
+          Your dream home is just a click away - start your journey to unparalleled living today!
+        </p>
+        <Link to={"/search"}>
+          <button className='p-4 bg-slate-700 text-white rounded-lg font-semibold'>Start now!</button>
+        </Link>
+      </div>
+      <div className='p-5 flex flex-col max-w-7xl mx-auto'>
+        <div className='p-5 flex flex-col'>
+          <h3 className='text-slate-700 text-3xl font-semibold'>Recent offers</h3>
+          <Link to={"/search?offer=true"} className=''>Show more offers</Link>
+          <div className='flex flex-col sm:flex-row gap-5 mt-5'>
+            {offersListingsCards}
+          </div>
+        </div>
+        <div className='p-5 flex flex-col'>
+          <h3 className='text-slate-700 text-3xl font-semibold'>Recent for rent</h3>
+          <Link to={"/search?rent=true"} className=''>Show more for rent</Link>
+          <div className='flex flex-col sm:flex-row gap-5 mt-5'>
+            {rentListingsCards}
+          </div>
+        </div>
+        <div className='p-5 flex flex-col'>
+          <h3 className='text-slate-700 text-3xl font-semibold'>Recent for sale</h3>
+          <Link to={"/search?sell=true"} className=''>Show more for sale</Link>
+          <div className='flex flex-col sm:flex-row gap-5 mt-5'>
+            {saleListingsCards}
+          </div>
+        </div>
+      </div>
+      
+    </div>
   )
 }
